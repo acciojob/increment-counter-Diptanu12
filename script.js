141dd61b8b06f38995722e6520e1b29382e155b9
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-05-15 06:47:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+  document.addEventListener('DOMContentLoaded', function () {
+	   const counterParagraph = document.getElementById('counter');
+            const incrementButton = document.getElementById('incrementBtn');
+	         incrementButton.addEventListener('click', function () {
+				 let currentValue = parseInt(counterParagraph.textContent);
+				     alert( currentValue);
+				  currentValue += 1;
+				      counterParagraph.textContent = currentValue;
+            });
+        });
